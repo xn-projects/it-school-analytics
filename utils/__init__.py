6fb34b6d8f9b3bab99c98feea41390cf955e3943
xn-projects <xeniachepigina@gmail.com>
2025-10-26 23:50:24 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -12,7 +12,7 @@ from .logging_setup import setup_logging, show_df, log_section
 from .data_summary import DataSummary
 from .cleaners import find_duplicates, clean_duplicates, convert_columns, frequent_non_null, clean_amount, normalize_german_level, convert_to_seconds, convert_to_minutes, convert_to_hours
 from .data_io import load_files, save_table_as_png, save_plot, save_clean_data
-from .my_palette import get_my_palette, cmap_cornflower, cmap_lime, cmap_tomato, cmap_yellow, cmap_lavender
+from .my_palette import get_my_palette, cmap_cornflower, cmap_lime, cmap_tomato, cmap_yellow, cmap_lavender, cmap_neutral
 from .descriptive_stats import describe_num, describe_cat, check_skewness, check_kurtosis, compare_distributions, plot_change
 
 __all__ = [
@@ -39,6 +39,7 @@ __all__ = [
     'cmap_tomato',
     'cmap_yellow',
     'cmap_lavender',
+    'cmap_neutral',
     'describe_num',
     'describe_cat',
     'check_skewness',
