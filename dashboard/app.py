f4e6d8451d142e31ed213001ea49491aeb0c7435
xn-projects <xeniachepigina@gmail.com>
2025-11-05 12:56:19 +0100

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -62,34 +62,29 @@ app.layout = dbc.Container([
     Input('product_filter', 'value')
 )
 def update_dashboard(selected_product):
-
     total_calls, total_deals, total_success = compute_kpi(deals, selected_product)
-    calls_card = make_card('Calls', int(total_calls), CALLS_COLOR)
-    deals_card = make_card('Deals', int(total_deals), DEALS_COLOR)
-    success_card = make_card('Success Deals', int(total_success), SUCCESS_COLOR)
+    calls_card = make_card("Calls", int(total_calls), CALLS_COLOR)
+    deals_card = make_card("Deals", int(total_deals), DEALS_COLOR)
+    success_card = make_card("Success Deals", int(total_success), SUCCESS_COLOR)
 
-    if selected_product == 'Total':
-        dfp = agg_product.groupby('Deal Created Month').sum(numeric_only=True).reset_index()
+    if selected_product == "Total":
+        dfp = agg_product.groupby("Deal Created Month").sum(numeric_only=True).reset_index()
         dfp['conversion'] = (dfp['success_count'] / dfp['deals_count'] * 100).fillna(0)
     else:
         dfp = agg_product[agg_product['Product'] == selected_product]
-
     fig_product = build_product_chart(dfp)
 
     df_edu = deals.copy()
-    if selected_product != 'Total':
+    if selected_product != "Total":
         df_edu = df_edu[df_edu['Product'] == selected_product]
 
     agg_edu_filtered = df_edu.groupby(['Deal Created Month', 'Education Type']).agg(
         deals_count=('Id', 'count'),
         success_count=('is_success', 'sum')
     ).reset_index()
+    agg_edu_filtered['conversion'] = (agg_edu_filtered['success_count'] / agg_edu_filtered['deals_count'] * 100).fillna(0)
 
-    agg_edu_filtered['conversion'] = (
-            agg_edu_filtered['success_count'] / agg_edu_filtered['deals_count'] * 100
-        ).fillna(0)
-
-    fig_edu = build_education_chart(agg_edu_filtered)
+    fig_edu = build_education_chart(agg_edu_filtered, "Education Type")
 
     return calls_card, deals_card, success_card, fig_product, fig_edu
 
