be44ec3f93f334399c6795d485f8711e8083d32e
xn-projects <xeniachepigina@gmail.com>
2025-11-05 12:29:03 +0100

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -93,5 +93,6 @@ def update_dashboard(selected_product):
 
     return calls_card, deals_card, success_card, fig_product, fig_edu
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8050, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(host="0.0.0.0", port=port)
