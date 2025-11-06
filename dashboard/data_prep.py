1e94ca6a563744bb49fd4e97f267676f052648ce
xn-projects <xeniachepigina@gmail.com>
2025-11-06 04:48:21 +0100

    Update data_prep.py

--- dashboard/data_prep.py
+++ dashboard/data_prep.py
@@ -37,8 +37,8 @@ def prepare_data(df_deals):
 
 def compute_kpi(df):
     total_deals = len(df)
-    success_deals = (df['Stage'].str.lower() == 'payment done').sum()
-    lost_deals = df['Stage'].str.lower() == 'lost').sum()
+    success_deals = (df['Stage'].str.lower().str.strip() == 'payment done').sum()
+    lost_deals = (df['Stage'].str.lower().str.strip() == 'lost').sum()
     closed_deals = df['Closing Date'].notna().sum()
 
     return total_deals, success_deals, lost_deals, closed_deals
