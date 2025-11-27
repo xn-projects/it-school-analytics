fb31146faebcd0f70bf0c841894e9fedea3b4204
xn-projects <xeniachepigina@gmail.com>
2025-11-28 00:54:47 +0100

    Update charts.py

--- dashboard/charts.py
+++ dashboard/charts.py
@@ -159,6 +159,7 @@ def build_payment_pie(df):
         template='plotly_white',
         height=500,
         margin=dict(l=40, r=40, t=60, b=40),
+        showlegend=False,
         font=dict(size=12, color='#333')
     )
 
